7836110e9cfbf903d877fe877d164d1b60266093
wangyong <wangyong@cmsr.chinamobile.com>
2022-12-01 14:45:31 +0800

    抽离头部和侧边栏

--- src/components/container/src/index.vue
+++ src/components/container/src/index.vue
@@ -2,29 +2,12 @@
   <el-container>
     <!-- 重点1： 宽度设置为自动 -->
     <el-aside width="auto">
-      <!-- 重点2： 控制开合和关闭 -->
-      <el-menu :collapse="isCollapse" default-active="2" class="el-menu-vertical-demo">
-        <el-menu-item index="1">
-          <el-icon><el-icon-menu /></el-icon>
-          <span>导航1</span>
-        </el-menu-item>
-        <el-menu-item index="2">
-          <el-icon><el-icon-menu /></el-icon>
-          <span>导航2</span>
-        </el-menu-item>
-        <el-menu-item index="3">
-          <el-icon><el-icon-menu /></el-icon>
-          <span>导航3</span>
-        </el-menu-item>
-      </el-menu>
+      <nav-side :collapse="isCollapse"></nav-side>
     </el-aside>
     <el-container>
       <el-header>
-        <span @click="toggle">
-          <el-icon-expand v-if="isCollapse"></el-icon-expand>
-          <el-icon-fold v-else></el-icon-fold>
-        </span>
-
+        <!-- 父子通信： 控制伸缩菜单 -->
+        <nav-header v-model:collapse="isCollapse"></nav-header>
       </el-header>
       <el-main>
         <RouterView></RouterView>
@@ -35,16 +18,14 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import navHeader from './navHeader/index.vue'
+import navSide from './navSide/index.vue'
 const isCollapse = ref(true)
-const toggle = () => {
-  isCollapse.value = !isCollapse.value
-  console.log(isCollapse.value)
-}
+
 </script>
 <style lang="scss" scoped>
-// 重点 3：  not 选择器
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-  min-height: 400px;
+.el-header {
+  padding: 0;
+  border-bottom: 1px solid #eee;
 }
 </style>
---
+++ src/components/container/src/navSide/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- 重点2： 控制开合和关闭 -->
+  <el-menu :collapse="collapse" default-active="2" class="el-menu-vertical-demo">
+    <el-menu-item index="1">
+      <el-icon><el-icon-menu /></el-icon>
+      <span>导航1</span>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <el-icon><el-icon-menu /></el-icon>
+      <span>导航2</span>
+    </el-menu-item>
+    <el-menu-item index="3">
+      <el-icon><el-icon-menu /></el-icon>
+      <span>导航3</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  collapse: boolean
+}>()
+
+
+
+
+</script>
+
+<style lang="scss" scoped>
+// 重点 3：  not 选择器
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  // min-height: 400px;
+}
+</style>
