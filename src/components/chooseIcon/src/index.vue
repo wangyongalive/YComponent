276f55cb29155db8259ec72081cd7dee78ef7199
wangyong <wangyong@cmsr.chinamobile.com>
2022-12-01 19:48:25 +0800

    图标选择器-通过自定义hooks函数实现复制功能

--- src/components/chooseIcon/src/index.vue
+++ src/components/chooseIcon/src/index.vue
@@ -4,8 +4,8 @@
   </el-button>
   <div class="y-choose-icon-dialog-body-height">
     <el-dialog :title="title" v-model="dialogVisible">
-      <div class="container">
-        <div class="item" v-for="(item) in Object.keys(ElementPlusIconsVue)" :key="item">
+      <div class="container" @click.stop="clickItem">
+        <div class="item" v-for="(item) in Object.keys(ElementPlusIconsVue)" :key="item" :data-item="item">
           <div class="icon">
             <component :is="`el-icon${toLine(item)}`"></component>
           </div>
@@ -20,7 +20,8 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
-import { toLine } from '../../../utls/index'
+import { toLine, eventProxy } from '../../../utls/index'
+import { useCopy } from '../../../hooks/useCopy'
 
 console.log(ElementPlusIconsVue)
 const props = defineProps<{
@@ -34,11 +35,26 @@ const emits = defineEmits(['update:visible'])
 // 拷贝一份父组件传递过来的visible
 let dialogVisible = ref<boolean>(props.visible)
 
+
+// 点击按钮显示弹出框
 const handleClick = () => {
   // 需要修改父组件的数据  emits
   emits('update:visible', !props.visible)
 }
 
+
+// 点击图标
+const clickItem = (e: any) => {
+  // 事件代理
+  let target = eventProxy(e, '.item')
+  // 获取文本
+  let text = `<el-icon-${toLine(target.dataset.item)} />`
+  // 复制文本
+  useCopy(text)
+  // 关闭弹框
+  dialogVisible.value = false
+
+}
 // 监听visible的变化 
 watch(() => props.visible, (val) => {
   dialogVisible.value = val
@@ -64,6 +80,7 @@ watch(() => dialogVisible.value, (val) => {
   flex-direction: column;
   align-items: center;
   margin-bottom: 20px;
+  cursor: pointer;
 }
 
 svg {
--- src/utls/index.ts
+++ src/utls/index.ts
@@ -2,3 +2,12 @@
 export const toLine = (value: string) => {
   return value.replace(/([A-Z])/g, '-$1').toLocaleLowerCase()
 }
+
+export const eventProxy = (e: any, query: string) => {
+  let target = e.target;
+  while (!target.matches(query)) {
+    target = target.parentNode;
+    if (e.currentTarget === target) return;
+  }
+  return target
+}
