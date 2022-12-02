1320d4e02f7fe95405528b601cfe5330a9e08566
wangyong <wangyong@cmsr.chinamobile.com>
2022-12-02 09:57:53 +0800

    [省市区选择组件-完善省市区联动组件并给父组件分发事件

--- src/components/chooseArea/src/index.vue
+++ src/components/chooseArea/src/index.vue
@@ -26,6 +26,18 @@
 import { ref, watch } from 'vue'
 import allAreas from '../lib/pca-code.json'
 
+// 定义数据的结构
+export interface AreaItem {
+  name: string,
+  code: string,
+  children?: AreaItem[]
+}
+
+export interface Data {
+  name: string,
+  code: string
+}
+
 // 下拉框选择省份的值
 let province = ref<string>('')
 // 下拉框选择城市的值
@@ -33,24 +45,62 @@ let city = ref<string>('')
 // 下拉框选择区域的值
 let area = ref<string>('')
 
-// selectCity computed 如果没有在模板中使用,就不会执行
-let selectCity = ref<any>([])
+// 城市下拉框的所有的值
+let selectCity = ref<AreaItem[]>([])
+
+// 区域下拉框的所有的值
+let selectArea = ref<AreaItem[]>([]);
+
+
+// 分发事件给父组件
+const emits = defineEmits(['change'])
+
+
+//  *****selectCity computed 如果没有在模板中使用,就不会执行
+
+
+// 监听选择省份
 watch(() => province.value, val => {
   if (val) {
-    selectCity.value = allAreas.find((item) => item.code === province.value)!.children
+    selectCity.value = allAreas.find((item) => item.code === val)!.children
   }
   city.value = ''
   area.value = ''
 })
 
-
-let selectArea = ref<any>([]);
+// 监听选择城市
 watch(() => city.value, val => {
   if (val) {
-    selectArea.value = selectCity.value.find((item: { code: string; }) => item.code === city.value)?.children
+    selectArea.value = selectCity.value.find((item) => item.code === val)!.children!
   }
   area.value = ''
 })
+
+// 监听选择区域
+watch(() => area.value, val => {
+  if (val) {
+    let proviceData: Data = {
+      code: province.value,
+      name: allAreas.find(item => item.code === province.value)!.name
+    }
+    let cityData: Data = {
+      code: city.value,
+      name: selectCity.value.find(item => item.code === city.value)!.name
+    }
+    let areaData: Data = {
+      code: area.value,
+      name: selectArea.value.find(item => item.code === area.value)!.name
+    }
+    emits('change', {
+      province: proviceData,
+      city: cityData,
+      area: areaData,
+    })
+  }
+
+})
+
+
 
 
 </script>
--- src/views/chooseArea/index.vue
+++ src/views/chooseArea/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
-    <choose-area></choose-area>
+    <choose-area @change="handleChange"></choose-area>
   </div>
 </template>
 
 <script setup lang="ts">
 import chooseArea from '../../components/chooseArea/src/index.vue'
 
+const handleChange = (data) => {
+  console.log(data)
+}
 </script>
 
 <style scoped>
