e51dddd647ac80e037d4fea88c419311ad95be57
wangyong <wangyong@cmsr.chinamobile.com>
2022-12-05 22:33:26 +0800

    城市选择组件-获取城市数据并显示所有城市

--- src/components/chooseCity/src/index.vue
+++ src/components/chooseCity/src/index.vue
@@ -1,12 +1,14 @@
 <template>
-  <el-popover placement="bottom-start" :width="430" trigger="click">
+
+  <el-popover placement="bottom-start" :width="430" trigger="click" v-model:visible="visible">
     <template #reference>
       <div class="result">
         <div>
           {{ result }}
         </div>
         <div>
-          <el-icon-arrow-up></el-icon-arrow-up>
+          <!-- rotate添加动画 -->
+          <el-icon-arrow-up :class="{ rotate: visible }"></el-icon-arrow-up>
         </div>
       </div>
     </template>
@@ -24,18 +26,36 @@
           </el-select>
         </el-col>
       </el-row>
+      <div class="city">
+        <div class="city-item" v-for="(val, key) in city.cities" :key="key">
+          {{ key }}</div>
+      </div>
+      <el-scrollbar max-height="300px">
+        <template v-for="(val, key) in city.cities">
+          <el-row>
+            <el-col :span="2">
+              <div>{{ key }}</div>
+            </el-col>
+            <el-col :span="22" class="city-name">
+              <div class="city-name-item" v-for="(item, index) in val" :key="item.id">{{ item.name }}</div>
+            </el-col>
+          </el-row>
+
+        </template>
+      </el-scrollbar>
     </div>
   </el-popover>
-
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import city from '../lib/city'
 // 最终选择的结果
 let result = ref<string>('请选择')
-
-const radioValue = ref<string>('按城市')
-
+// 控制弹出层的显示
+let visible = ref<boolean>(false)
+// 单选框的值 按城市还是按省份选择
+let radioValue = ref<string>('按城市')
 // 下拉框的值 搜索下拉框
 let selectValue = ref<string>('')
 const options = [
@@ -63,15 +83,58 @@ const options = [
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .result {
   display: flex;
   align-items: center;
   width: fit-content;
   cursor: pointer;
+
+  svg {
+    width: 1em;
+    height: 1em;
+    position: relative;
+    top: 2px;
+    margin-left: 4px;
+    transition: all 0.25s linear;
+
+    &.rotate {
+      transform: rotate(180deg);
+    }
+  }
+
+
 }
 
 .container {
   padding: 6px;
+
+  .city {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    margin-top: 10px;
+    margin-bottom: 10px;
+
+    &-item {
+      padding: 3px 6px;
+      margin-right: 8px;
+      border: 1px solid #eee;
+      margin-bottom: 8px;
+      cursor: pointer;
+    }
+  }
+
+  .city-name {
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+
+    .city-name-item {
+      margin-right: 6px;
+      margin-bottom: 6px;
+      cursor: pointer;
+    }
+  }
 }
 </style>
